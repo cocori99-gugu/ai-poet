120656c2c0e79cc61890fb46bb1c8d1e6687712b
cocori99-gugu <cocori99@gmail.com>
2025-12-07 19:21:18 +0900

    Disable dotenv loading in main.py
    Comment out dotenv import and loading.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 
 #ChatOpenAI 초기화
